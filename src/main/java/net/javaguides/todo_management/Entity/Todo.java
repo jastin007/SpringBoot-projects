3dbf591f350a987e89ce96557935cef385e3d2d4
Arun Kumar <arun.kumar@gmail.com>
2025-01-09 09:58:17 +0530

    initial commit

---
+++ src/main/java/net/javaguides/todo_management/Entity/Todo.java
@@ -0,0 +1,33 @@
+package net.javaguides.todo_management.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+    private boolean completed;
+
+}
---
+++ src/main/java/net/javaguides/todo_management/dto/Tododto.java
@@ -0,0 +1,18 @@
+package net.javaguides.todo_management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tododto {
+    private long id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+}
