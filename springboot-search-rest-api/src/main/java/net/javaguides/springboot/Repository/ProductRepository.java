b6a6813505d81405dcbdd3e90dd8d6440a4213de
Jastin Samuvel A <arun.kumar@gmail.com>
2025-01-27 15:09:07 +0530

    Product repo update query param

--- springboot-search-rest-api/src/main/java/net/javaguides/springboot/Repository/ProductRepository.java
+++ springboot-search-rest-api/src/main/java/net/javaguides/springboot/Repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package net.javaguides.springboot.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,22 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p WHERE " + "p.name LIKE CONCAT('%',:query,'%')" +
             "Or p.description LIKE CONCAT('%', :query, '%')")
     List<Product> searchProducts(String query);
+
+    public Product findByName(String name);
+
+    // Optional<Product> findById(Long id);
+    Optional<Product> findById(Long id);
+
+    List<Product> findByNameOrDescription(String name, String description);
+
+    Product findDistinctByName(String name);
+
+    List<Product> findByNameContaining(String name);
+
+    List<Product> findByNameLike(String name);
+
+    List<Product> findByNameIn(List<String> name);
+
+    List<Product> findFirst2ByOrderByNameAsc();
+
 }
