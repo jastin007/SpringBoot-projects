6faafc76c29fa98279b08a5d81da0f37c920bcc0
Jastin Samuvel A <arun.kumar@gmail.com>
2025-02-17 15:02:50 +0530

    Removed .history from tracking

---
+++ springboot-webflux-tutorial/src/test/java/net/javaguides/springboot_webflux_tutorial/SpringbootWebfluxTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot_webflux_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
