adac1bf5797021c9a68cc0bb2d49940f5bf16c3f
Jastin Samuvel A <arun.kumar@gmail.com>
2025-05-22 11:49:59 +0530

    Removed extra Spring Boot files

--- src/main/java/net/javaguides/ems_backend/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package net.javaguides.ems_backend.Repository;
-
-import jakarta.persistence.Id;
-import net.javaguides.ems_backend.Entity.Employees;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employees, Long> {
-}
